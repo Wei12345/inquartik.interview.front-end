e10116a52343bd2c6285cad19c4f6264d34987c7
Wei <wwwwwwwx2001@gmail.com>
2021-01-08 05:09:05 +0800

    feat: unique number start offset

--- src/libs/unique.js
+++ src/libs/unique.js
@@ -1,6 +1,14 @@
-let currentNumber = -1;
+import { ORDER_LOCAL_STORAGE_KEY } from '@/constants/storage'
+
+const orderData = JSON.parse(localStorage.getItem(ORDER_LOCAL_STORAGE_KEY)) || [];
+
+const numberOrderIds = orderData.map(order => order.id)
+const numberOrderNoteIds = orderData.map(order => order.notes.map(note => note.id)).flat();
+const maxNumberId = Math.max(...numberOrderIds, ...numberOrderNoteIds);
+
+let currentNumber = maxNumberId + 1;
 
 export function uniqueNumber() {
-    currentNumber += 1;
-    return `${currentNumber}`;
+  currentNumber += 1;
+  return `${currentNumber}`;
 }
