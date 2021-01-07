721616e88b0a18e2fe012205fe057c368bbde69c
Wei <wwwwwwwx2001@gmail.com>
2021-01-07 23:06:57 +0800

    feat: add navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar">
+    <router-link to="/">Home</router-link>
+    <router-link to="/Add">Add</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+}
+</script>
+
+<style lang="scss" scoped>
+  .navbar {
+    & > a + a {
+      margin-left: 15px;
+    }
+  }
+</style>
--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="default-layout">
-    <router-link to="/">Home</router-link>
-    <router-link to="/Add">Add</router-link>
+    <Navbar />
     <router-view />
   </div>
 </template>
 
 <script>
+import Navbar from '@/components/Navbar';
+
 export default {
   name: 'DefaultLayout',
+  components: {
+    Navbar
+  }
 }
 </script>
 
